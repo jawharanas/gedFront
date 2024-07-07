2bc01bf5a369332da28e38e83d46a2ee7257b1a2
EL ARFAOUI Mohamed <mohamed.elarfaoui@orange.com>
2024-07-07 20:21:30 +0100

    add login error message

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,13 @@
 import GoogleButton from "./GoogleButton";
 import GithubButton from "./GithubButton";
-import {useState} from "react";
+import { useState } from "react";
 import axios from "axios";
 import { useAuthService } from "../Services/AuhtenticationService";
 
 const Login: React.FC = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [error, setError] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
   // Destructure login from useAuthService
   const { login } = useAuthService();
@@ -26,8 +26,7 @@ const Login: React.FC = () => {
   };
 
   const handleGitHubLogin = () => {
-    const clientId =
-      "e18566d3dcfda4252dc8";
+    const clientId = "e18566d3dcfda4252dc8";
     const redirectUri = "http://localhost:5173/callback"; // Your redirect URI
     const scope = "profile email";
     const state = "random_state_string"; // CSRF protection
@@ -39,29 +38,33 @@ const Login: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setError('');
+    setError("");
 
     try {
       // Use the login method from useAuthService instead of direct axios call
       const response = await login({ email, password });
-      console.log('Login successful:', response);
-      window.location.href ="http://localhost:5173/Homepage"
-      
+      console.log("Login successful:", response);
+      window.location.href = "http://localhost:5173/Homepage";
     } catch (error: any) {
-      console.error('Login failed:', error.message);
+      console.error("Login failed:", error.message);
       setError(error.message);
     }
   };
 
   return (
     <div className="flex flex-col height:500vh;">
-      <div className="flex justify-centre items-centre w  ">
-        
-      </div>
+      <div className="flex justify-centre items-centre w  "></div>
       <div className="flex flex-grow justify-center items-center bg-white-100">
-        <form className="w-[370px] p-4 bg-white border-2 border-grey-100 px-5 py-15 rounded-3xl border-1 bordergray mb-3" onSubmit={handleSubmit}>
+        <form
+          className="w-[370px] p-4 bg-white border-2 border-grey-100 px-5 py-15 rounded-3xl border-1 bordergray mb-3"
+          onSubmit={handleSubmit}
+        >
           <div className="flex justify-center items-center w-full mb-10">
-            <img src="\download.png" alt="Logo" className=" flex justify-right items-right  h-19 w-19 md:h-21 md:w-21 lg:h-22 lg:w-22 mb-8 " />
+            <img
+              src="\download.png"
+              alt="Logo"
+              className=" flex justify-right items-right  h-19 w-19 md:h-21 md:w-21 lg:h-22 lg:w-22 mb-8 "
+            />
           </div>
           <GoogleButton handleClick={handleGoogleLogin} />
 
@@ -82,13 +85,21 @@ const Login: React.FC = () => {
             className="w-full p-2 bg-grey-500 text-black rounded-md mb-4 hover:bg-red-100 transition-colors duration-20"
             required
           />
-          
-          <button type="submit"
+
+          <button
+            type="submit"
             className="w-full p-2 bg-blue-500 text-white rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 active:bg-blue-700"
-          > Login
+          >
+            {" "}
+            Login
           </button>
         </form>
       </div>
+      {error && (
+        <div className="text-red-500 flex justify-center">
+          Authentication error , please try again !{" "}
+        </div>
+      )}
     </div>
   );
 };
